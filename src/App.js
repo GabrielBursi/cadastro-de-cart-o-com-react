4e6a114990d5905ea34a776a2834b2156fab99b1
Gabriel Bursi <gabriellbursii@gmail.com>
2022-12-01 11:02:56 -0300

    criando e estilo do componente dos cartoes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './global.css'
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='container-geral'>
-      
+      <HomePage/>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,27 @@
+import Logo from "../images/card-logo.svg";
+import './card.css'
+
 function Card() {
     return (
         <aside>
-            
+            <div className='container-cards'>
+                <div className='card-front'>
+                    <div className='icon'>
+                        <img src={Logo} alt='logo'/>
+                    </div>
+                    <div className='numero'>
+                        <p>0000 0000 0000 0000</p>
+                    </div>
+                    <div className='nome-data'>
+                        <span>GABRIEL BURSI</span>
+                        <span>00/00</span>
+                    </div>
+                </div>
+                
+                <div className='card-back'>
+
+                </div>
+            </div>
         </aside>
     );
 }
