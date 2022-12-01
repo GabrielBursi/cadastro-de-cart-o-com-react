8aead985e707c2a5f2c45d4a1868e757a8977604
Gabriel Bursi <gabriellbursii@gmail.com>
2022-12-01 19:35:37 -0300

    corrigindo input de data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,17 +5,20 @@ function Form() {
         <form>
             <div className="form-container">
                 <label htmlFor="cardholder">CARDHOLDER NAME</label>
-                <input id="cardholder" type="text" autoComplete='off'/>
+                <input id="cardholder" type="text" autoComplete='off' placeholder='Gabriel Bursi'/>
                 <label htmlFor="card-number">CARD NUMBER</label>
-                <input id="card-number" type="number"/>
+                <input id="card-number" type="card" placeholder='0000 0000 0000 0000'/>
                 <div className="data-cvc">
                     <div className='date'>
                         <label htmlFor="date">EXP. DATE (MM/YY)</label>
-                        <input id="date" type="date"/>
+                        <div className='inputs-my'>
+                            <input id="date" type="number" placeholder='MM'/>
+                            <input id="date" type="number" placeholder='YY'/>
+                        </div>
                     </div>
                     <div className='cvc-form'>
                         <label htmlFor="cvc-form">CVC</label>
-                        <input id="cvc-form" type="number"/>
+                        <input id="cvc-form" type="number" placeholder='123' max='3'/>
                     </div>
                 </div>
                 <Button nav='/complete' text='Confirm'/>
