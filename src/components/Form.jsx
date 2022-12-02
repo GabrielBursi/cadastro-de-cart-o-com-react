22c8d824df73e740e5fecc42227be5f209ece260
Gabriel Bursi <gabriellbursii@gmail.com>
2022-12-01 22:30:30 -0300

    controle de caracteres

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,7 @@ import Button from './Button';
 function Form() {
 
     const {
+        name,
         setName,
         setNumber,
         setMounth,
@@ -17,61 +18,70 @@ function Form() {
         return e === '';
     }
 
+    function filterInput(e){
+        const regexStr = /^[a-zA-Z\s]+$/
+        if(!e.target.value.match(regexStr)){
+            if(name === "Gabriel Bursi"){
+                e.target.value = ''
+            }else{
+
+                e.target.value = name
+            }
+        }
+    }
+
     function getInputs() {
         const inputsNodeList = document.querySelectorAll("input");
         return inputsNodeList
     }
+
     return (
         <form>
             <div className="form-container">
                 <label htmlFor="cardholder">CARDHOLDER NAME</label>
-                <input id="cardholder" type="text" autoComplete='off' placeholder='Gabriel Bursi' onChange={(e) => {
+                <input id="cardholder" type="text" autoComplete='off' placeholder='ex: Gabriel Bursi' onChange={(e) => {
                         if(isEmpty(e.target.value)) {
                             setName('Gabriel Bursi')
                         }else{
-
+                            filterInput(e);
                             setName(e.target.value)
                         }
                     }}/>
                 <label htmlFor="card-number">CARD NUMBER</label>
-                <input id="card-number" type="card" placeholder='0000 0000 0000 0000' onChange={(e) => {
+                <input id="card-number" type="number" placeholder='0000 0000 0000 0000' maxLength={16} autoComplete='off' onChange={(e) => {
                         if(isEmpty(e.target.value)) {
                             setNumber('0000 0000 0000 0000')
                         }else{
-
-                            setNumber(e.target.value)
+                            setNumber(e.target.value.slice(0,16))
                         }
                     }}/>
                 <div className="data-cvc">
                     <div className='date'>
                         <label htmlFor="date">EXP. DATE (MM/YY)</label>
                         <div className='inputs-my'>
-                            <input id="date" type="number" placeholder='MM' onChange={(e) => {
+                            <input id="date" type="number" placeholder='MM' maxLength={2} autoComplete='off' onChange={(e) => {
                                 if(isEmpty(e.target.value)) {
                                     setMounth('00')
                                 }else{
-
-                                    setMounth(e.target.value)
+                                    setMounth(e.target.value.slice(0, 2));
                                 }
                             }}/>
-                            <input id="date" type="number" placeholder='YY' onChange={(e) => {
+                            <input id="date" type="number" placeholder='YY' maxLength={2} autoComplete='off' onChange={(e) => {
                                 if(isEmpty(e.target.value)) {
                                     setYear('00')
                                 }else{
-
-                                    setYear(e.target.value)
+                                    setYear(e.target.value.slice(0,2))
                                 }
                             }}/>
                         </div>
                     </div>
                     <div className='cvc-form'>
                         <label htmlFor="cvc-form">CVC</label>
-                        <input id="cvc-form" type="number" placeholder='123' onChange={(e) => {
+                        <input id="cvc-form" type="number" placeholder='123' maxLength={3} autoComplete='off' onChange={(e) => {
                             if(isEmpty(e.target.value)) {
                                 setCVC('000')
                             }else{
-
-                                setCVC(e.target.value)
+                                setCVC(e.target.value.slice(0, 3));
                             }
                         }}/>
                     </div>
